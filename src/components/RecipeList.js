c4ebeafac43e2bf7b532662214192c4cdd38bf65
Artiwarah Damrongchai <artiwarah@iwa.fi>
2017-07-31 16:08:08 +0700

    Making recipe items

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import RecipeItem from './RecipeItem';
 
 class RecipeList extends Component {
   render(){
-    console.log('props', this.props);
-
     return(
       <div>
         <h4>Recipe List</h4>
-        <ul className='list-group'>
-          {
-            this.props.recipes.map((recipe, index) => {
-              return(
-                <li className='list-group-item' key={index}>
-                  <div className='list-item'>{recipe.title}</div>
-                </li>
-              )
-            })
-          }
-        </ul>
+        {
+          this.props.recipes.map((recipe, index) => {
+            return(
+              <RecipeItem key={index} recipe={recipe} />
+            )
+          })
+        }
       </div>
     )
   }
